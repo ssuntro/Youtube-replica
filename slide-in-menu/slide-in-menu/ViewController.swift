0f598c2c4e5b66999dbfeec7e0abfd11e0ea55b8
Siroratt Suntronsuk <ann@odds.team>
2019-10-25 14:39:44 +0700

    initial project

---
+++ slide-in-menu/slide-in-menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slide-in-menu
+//
+//  Created by siroratt suntronsuk on 25/10/2562 BE.
+//  Copyright © 2562 siroratt suntronsuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
